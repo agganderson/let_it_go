6883175ffc494859c01dabb2b8f4a392afdadcdc
Alexandra Anderson <aliganderson@gmail.com>
2016-03-11 12:24:52 -0800

    initial commit

---
+++ application/models/Let.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+class Let extends CI_Model{
+	
+}
+
+?>
---
+++ application/views/dashboard.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset='utf-8'>
+	<title>Dashboard</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+	integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" 
+	integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
+	integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
+	</script>
+
+	<link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
+
+
+</head>
+<body>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">WebSiteName</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li class="active"><a href="#">Home</a></li>
+	    </ul>
+	    <ul class="nav navbar-nav navbar-right">
+	    	<li><a href="#"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+	    	<li><a href="#"><span class="glyphicon glyphicon-user"></span>Profile</a></li>
+	    </ul>
+	  </div>
+	</nav>
+
+	<div class='container'>
+
+		<div id="main" class="col-md-7">
+			<h1>Main complain page</h1>
+			 <form action="#" method="POST" role="form" class="col-md-6">
+				<div class="form-group">
+					<label for="sel1">What do you need to vent about?</label>
+					<select class="form-control" id="sel1">
+						<option>Work</option>
+						<option>School</option>
+						<option>Relationships</option>
+						<option>Family</option>
+						<option>Money</option>
+						<option>Miscellaneous</option>
+					</select>
+				</div>
+				<div class="form-group">
+			  		<textarea class="form-control" rows="5" id="comment" name="comment"></textarea>
+				</div>
+			    <button type="submit" class="btn btn-danger">Vent</button>
+			</form>
+			
+			<div id="vents">
+				<h1>VENTS ARE GONNA GO HERE</h1>
+				<p>Ignorant saw her her drawings marriage laughter. Case oh an that or away sigh do here upon. Acuteness you exquisite ourselves now end forfeited. Enquire ye without it garrets up himself. Interest our nor received followed was. Cultivated an up solicitude mr unpleasant. 
+
+Alteration literature to or an sympathize mr imprudence. Of is ferrars subject as enjoyed or tedious cottage. Procuring as in resembled by in agreeable. Next long no gave mr eyes. Admiration advantages no he celebrated so pianoforte unreserved. Not its herself forming charmed amiable. Him why feebly expect future now. 
+
+Living valley had silent eat merits esteem bed. In last an or went wise as left. Visited civilly am demesne so colonel he calling. So unreserved do interested increasing sentiments. Vanity day giving points within six not law. Few impression difficulty his use has comparison decisively. 
+
+Kindness to he horrible reserved ye. Effect twenty indeed beyond for not had county. The use him without greatly can private. Increasing it unpleasant no of contrasted no continuing. Nothing colonel my no removed in weather. It dissimilar in up devonshire inhabiting. 
+
+Real sold my in call. Invitation on an advantages collecting. But event old above shy bed noisy. Had sister see wooded favour income has. Stuff rapid since do as hence. Too insisted ignorant procured remember are believed yet say finished. 
+
+Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least. 
+
+Now residence dashwoods she excellent you. Shade being under his bed her. Much read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant horrible but confined day end marriage. Eagerness furniture set preserved far recommend. Did even but nor are most gave hope. Secure active living depend son repair day ladies now. 
+
+Do play they miss give so up. Words to up style of since world. We leaf to snug on no need. Way own uncommonly travelling now acceptance bed compliment solicitude. Dissimilar admiration so terminated no in contrasted it. Advantages entreaties mr he apartments do. Limits far yet turned highly repair parish talked six. Draw fond rank form nor the day eat. 
+
+Day handsome addition horrible sensible goodness two contempt. Evening for married his account removal. Estimable me disposing of be moonlight cordially curiosity. Delay rapid joy share allow age manor six. Went why far saw many knew. Exquisite excellent son gentleman acuteness her. Do is voice total power mr ye might round still. 
+
+He an thing rapid these after going drawn or. Timed she his law the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant formerly so ye blessing. He as spoke avoid given downs money on we. Of properly carriage shutters ye as wandered up repeated moreover. Inquietude attachment if ye an solicitude to. Remaining so continued concealed as knowledge happiness. Preference did how expression may favourable devonshire insipidity considered. An length design regret an hardly barton mr figure. 
+</p>
+			</div>
+
+		</div>
+
+		<div id="right" class="col-md-4">
+			<h1>Categories</h1>
+			<div id="categories">
+				 <div class="list-group">
+				  <a href="#" class="list-group-item">Work</a>
+				  <a href="#" class="list-group-item">School</a>
+				  <a href="#" class="list-group-item">Relationships</a>
+				  <a href="#" class="list-group-item">Family</a>
+				  <a href="#" class="list-group-item">Money</a>
+				  <a href="#" class="list-group-item">Miscellaneous</a>
+				</div>
+			</div>
+
+			<div id="twitter">
+				<h1>Twitter Feed</h1>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+				<p>TWEET HERE</p>
+			</div>
+
+		</div>
+
+	</div>
+</body>
+</html>
